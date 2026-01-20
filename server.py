6902ba85d2506ad61897825ca481caa2c307163e
Shorokhovv <130422961+Shorokhovv@users.noreply.github.com>
2026-01-20 12:52:05 +0500

    2nt_pull

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Веб-технологии</title>
+    <title>Я сделяль</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <style>
         body {{
